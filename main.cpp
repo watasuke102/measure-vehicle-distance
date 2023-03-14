1b026c65fe960b831744b446ed7ccbf63869891f
watasuke102 <watasuke102@gmail.com>
2023-03-14 14:53:16 +0800

    update: undistort the frame from camera

--- main.cpp
+++ main.cpp
@@ -3,14 +3,19 @@
 #include <opencv/cv.hpp>
 
 int main() {
+  cv::Mat camera_mat, dist_coeffs;
+  {
+    cv::FileStorage fs("camera.yml", cv::FileStorage::READ);
+    fs["camera_mat"] >> camera_mat;
+    fs["dist_coeffs"] >> dist_coeffs;
+    fs.release();
+  }
+
   auto camera = cv::VideoCapture(0);
   if (!camera.isOpened()) {
     std::cerr << "[Fatal] cannot open the camera" << std::endl;
     return 1;
   }
-
-  std::vector<std::vector<cv::Point3f>> obj_points;
-  std::vector<std::vector<cv::Point2f>> img_points;
 
   cv::Mat frame;
   while(true) {
@@ -19,8 +24,12 @@ int main() {
       std::cerr << "[Fatal] cannot read from the camera" << std::endl;
       return 1;
     }
-    cv::imshow("camera", frame);
-    if (cv::waitKey(10) != -1) {
+
+    cv::Mat undistorted_image;
+    cv::undistort(frame, undistorted_image, camera_mat, dist_coeffs);
+    cv::imshow("camera", undistorted_image);
+
+    if (cv::waitKey(1) != -1) {
       break;
     }
   }
