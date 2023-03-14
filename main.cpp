3bde9234cccbf6bb723b0f09f3c87d78cae9de27
watasuke102 <watasuke102@gmail.com>
2023-03-14 15:31:13 +0800

    update: retrieve red value from the image

--- main.cpp
+++ main.cpp
@@ -17,6 +17,10 @@ int main() {
     return 1;
   }
 
+  int threshold = 160;
+  cv::namedWindow("camera", cv::WINDOW_FULLSCREEN | cv::WINDOW_KEEPRATIO | cv::WINDOW_GUI_EXPANDED);
+  cv::createTrackbar("red_threshold", "camera", &threshold, 255);
+
   cv::Mat frame;
   while(true) {
     camera.read(frame);
@@ -27,7 +31,15 @@ int main() {
 
     cv::Mat undistorted_image;
     cv::undistort(frame, undistorted_image, camera_mat, dist_coeffs);
-    cv::imshow("camera", undistorted_image);
+    cv::imshow("camera (original)", undistorted_image);
+
+    cv::Mat bgr[3];
+    cv::split(undistorted_image, bgr);
+
+    cv::Mat mono;
+    cv::threshold(bgr[2], mono, threshold, 255, cv::THRESH_BINARY);
+
+    cv::imshow("camera", mono);
 
     if (cv::waitKey(1) != -1) {
       break;
