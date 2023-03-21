3bf6c098dfc63ee337a84ee463bddd3c09188f73
watasuke102 <watasuke102@gmail.com>
2023-03-21 11:12:51 +0800

    update: read frame from avi file

--- main.cpp
+++ main.cpp
@@ -11,29 +11,30 @@ int main() {
     fs.release();
   }
 
-  auto camera = cv::VideoCapture(0);
-  if (!camera.isOpened()) {
-    std::cerr << "[Fatal] cannot open the camera" << std::endl;
+  // cv::VideoCapture source(0);
+  cv::VideoCapture source("video.avi");
+  if (!source.isOpened()) {
+    std::cerr << "[Fatal] cannot open the source" << std::endl;
     return 1;
   }
 
   int threshold = 160, distance = 0;
-  cv::namedWindow("camera", cv::WINDOW_FULLSCREEN | cv::WINDOW_KEEPRATIO | cv::WINDOW_GUI_EXPANDED);
-  cv::createTrackbar("red_threshold", "camera", &threshold, 255);
-  cv::createTrackbar("distance", "camera", NULL, 10000);
+  cv::namedWindow("source", cv::WINDOW_FULLSCREEN | cv::WINDOW_KEEPRATIO | cv::WINDOW_GUI_EXPANDED);
+  cv::createTrackbar("red_threshold", "source", &threshold, 255);
+  cv::createTrackbar("distance", "source", NULL, 10000);
 
   cv::Mat frame;
   while(true) {
-    cv::setTrackbarPos("distance", "camera", ++distance);
-    camera.read(frame);
+    cv::setTrackbarPos("distance", "source", ++distance);
+    source.read(frame);
     if (frame.empty()) {
-      std::cerr << "[Fatal] cannot read from the camera" << std::endl;
+      std::cerr << "[Fatal] cannot read from the source" << std::endl;
       return 1;
     }
 
     cv::Mat undistorted_image;
     cv::undistort(frame, undistorted_image, camera_mat, dist_coeffs);
-    cv::imshow("camera (original)", undistorted_image);
+    cv::imshow("source (original)", undistorted_image);
 
     cv::Mat bgr[3];
     cv::split(undistorted_image, bgr);
@@ -41,19 +42,19 @@ int main() {
     cv::Mat mono;
     cv::threshold(bgr[2], mono, threshold, 255, cv::THRESH_BINARY);
 
-    cv::imshow("camera", mono);
+    cv::imshow("source", mono);
 
-    std::vector<std::vector<cv::Point>> contours, contours_poly;
-    cv::findContours(mono, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    std::vector<cv::Rect> bound_rect;
-    for (int i=0; i < contours.size(); ++i) {
-      cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
-      bound_rect.push_back(cv::boundingRect(contours_poly[i]));
-    }
+    // std::vector<std::vector<cv::Point>> contours, contours_poly;
+    // cv::findContours(mono, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    // std::vector<cv::Rect> bound_rect;
+    // for (int i=0; i < contours.size(); ++i) {
+    //   cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
+    //   bound_rect.push_back(cv::boundingRect(contours_poly[i]));
+    // }
 
-    for (auto rect : bound_rect) {
-      std::cout << rect << std::endl;
-    }
+    // for (auto rect : bound_rect) {
+    //   std::cout << rect << std::endl;
+    // }
 
     if (cv::waitKey(1) != -1) {
       break;
