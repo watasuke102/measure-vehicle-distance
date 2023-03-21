416f0b204bd92a0f9d2020f22fbce7d8edf266a0
watasuke102 <watasuke102@gmail.com>
2023-03-21 09:46:02 +0800

    wip

--- main.cpp
+++ main.cpp
@@ -17,12 +17,14 @@ int main() {
     return 1;
   }
 
-  int threshold = 160;
+  int threshold = 160, distance = 0;
   cv::namedWindow("camera", cv::WINDOW_FULLSCREEN | cv::WINDOW_KEEPRATIO | cv::WINDOW_GUI_EXPANDED);
   cv::createTrackbar("red_threshold", "camera", &threshold, 255);
+  cv::createTrackbar("distance", "camera", NULL, 10000);
 
   cv::Mat frame;
   while(true) {
+    cv::setTrackbarPos("distance", "camera", ++distance);
     camera.read(frame);
     if (frame.empty()) {
       std::cerr << "[Fatal] cannot read from the camera" << std::endl;
@@ -40,6 +42,18 @@ int main() {
     cv::threshold(bgr[2], mono, threshold, 255, cv::THRESH_BINARY);
 
     cv::imshow("camera", mono);
+
+    std::vector<std::vector<cv::Point>> contours, contours_poly;
+    cv::findContours(mono, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    std::vector<cv::Rect> bound_rect;
+    for (int i=0; i < contours.size(); ++i) {
+      cv::approxPolyDP(contours[i], contours_poly[i], 3, true);
+      bound_rect.push_back(cv::boundingRect(contours_poly[i]));
+    }
+
+    for (auto rect : bound_rect) {
+      std::cout << rect << std::endl;
+    }
 
     if (cv::waitKey(1) != -1) {
       break;
